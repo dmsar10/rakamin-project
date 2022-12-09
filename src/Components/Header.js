446435b816ff0dedd22ffe67562756115543373b
Dimas Arya <dimasarya96@gmail.com>
2022-12-09 14:32:11 +0700

    Tailwind css setup
    Header and Footer added using tailwind

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header(){
+    return (
+        <header className="border-b-4 border-black font-bold p-5">
+            Joki
+        </header>
+    )
+}
+
+export default Header
